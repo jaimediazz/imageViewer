98adf3e98766c677e1326fb397e2f54185fbf402
hp <hp@192.168.56.1>
2020-01-15 11:37:12 +0100

    MVC done

---
+++ src/imageviewer/Command.java
@@ -0,0 +1,7 @@
+package imageviewer;
+
+public interface Command {
+    
+    public void execute();
+    
+}
--- src/imageviewer/Image.java
+++ src/imageviewer/Image.java
@@ -3,6 +3,7 @@ package imageviewer;
 public interface Image {
     
     public String getName();
+    public byte[] data();
     public Image next();
     public Image prev();
     
